93f3a09cbe5ce5d5ad10e313223091970dc4a013
Smit Koli <smit.koli@morningstar.com>
2021-08-09 12:19:17 +0530

    uc7 completed for creating employee class

--- wage.js
+++ wage.js
@@ -125,3 +125,93 @@ console.log("----------uc6---------");
     console.log("Total Working Days = " +current_day);
     console.log("Monthly Employee Wage = " +total_employee_wage);
 }
+
+//uc7
+console.log("----------uc7---------");
+class Employee{
+    constructor(){
+        console.log("Welcome to employee wage computation");
+    }
+    checkAttendance(){
+        let key = Math.floor(Math.random()*10%3);
+        switch(key){
+            case 0:
+                return 0;
+                break;
+            case 1:
+                return 1;
+                break;
+            case 2:
+                return 2;
+                break;
+            default:
+                console.log("In default case");
+                break;
+        }
+    }
+    partTimeWage(){
+        return part_time_wage*wage_per_hr;
+    }
+    fullTimeWage(){
+        return full_time_wage*wage_per_hr;
+    }
+    absentWage(){
+        return 0;
+    }
+    dailyWage(attendance){
+        if(attendance == 0){
+            return this.partTimeWage();
+        }
+        else if(attendance == 1){
+            return this.fullTimeWage();
+        }
+        else{
+            return this.absentWage();
+        }
+    }
+    monthlyWage(){
+        let total_wage = 0;
+        for(let i=0;i<days_in_month;i++){
+            total_wage += this.dailyWage(this.checkAttendance());
+        }
+        return total_wage;
+    }
+    restrictedHours(max_working_hours){
+        let total_wage = 0;
+        let current_hours = 0;
+        let current_day = 0;
+        let total_part_time_days = 0;
+        let total_full_time_days = 0;
+        let total_absent_time_days = 0;
+
+        while(current_hours <= max_working_hours && current_day < days_in_month){
+            let wage = this.dailyWage(this.checkAttendance());
+            total_wage += wage;
+            if(wage == 160){
+                total_full_time_days += 1;
+                current_hours += 8;
+            }
+            else if(wage == 80){
+                total_part_time_days += 1;
+                current_hours += 4;
+            }
+            else{
+                total_absent_time_days += 1;
+                current_hours += 0;
+            }
+            current_day += 1;
+        }
+
+        console.log("Total Absent Days = " +total_absent_time_days);
+        console.log("Total Part Time Days = " +total_part_time_days);
+        console.log("Total Full Time Days = " +total_full_time_days);
+        console.log("Total Working Hours = " +current_hours);
+        console.log("Total Working Days = " +current_day);
+        console.log("Monthly Employee Wage = " +total_wage);
+    }
+}
+
+var emp = new Employee();
+console.log("Monthly wage = " +emp.monthlyWage());
+console.log("-----------------------------------");
+emp.restrictedHours(160);

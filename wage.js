d7c13805ec9e4772ff7949c477e9cfddc3630ea1
Smit Koli <smit.koli@morningstar.com>
2021-08-09 11:09:14 +0530

    uc4 completed to calculate employee wage using switch

--- wage.js
+++ wage.js
@@ -5,23 +5,24 @@ const wage_per_hr = 20
 //uc1
 console.log("----------uc1---------");
 console.log("Welcome to employee wage computation");
-
-let key = Math.floor(Math.random()*10%3);
-        let isfulltime = false;
-        switch(key){
-            case 0:
-                console.log("Part time employee");
-                break;
-            case 1:
-                console.log("Full time employee");
-                break;
-            case 2:
-                console.log("Employee absent");
-                break;
-            default:
-                console.log("In default case");
-                break;
-        }
+{
+    let key = Math.floor(Math.random()*10%3);
+    let isfulltime = false;
+    switch(key){
+        case 0:
+            console.log("Part time employee");
+            break;
+        case 1:
+            console.log("Full time employee");
+            break;
+        case 2:
+            console.log("Employee absent");
+            break;
+        default:
+            console.log("In default case");
+            break;
+    }
+}
 
 //uc2
 console.log("----------uc2---------");
@@ -30,3 +31,24 @@ console.log("Full time wage for employee is : " +full_time_wage*wage_per_hr);
 //uc3
 console.log("----------uc3---------");
 console.log("Part time wage for employee is : " +part_time_wage*wage_per_hr);
+
+//uc4
+console.log("----------uc4---------");
+{
+    let key = Math.floor(Math.random()*10%3);
+        let isfulltime = false;
+        switch(key){
+            case 0:
+                console.log("Part time wage for employee is : " +part_time_wage*wage_per_hr);
+                break;
+            case 1:
+                console.log("Full time wage for employee is : " +full_time_wage*wage_per_hr);
+                break;
+            case 2:
+                console.log("Absent wage for employee is : 0");
+                break;
+            default:
+                console.log("In default case");
+                break;
+        }
+}
